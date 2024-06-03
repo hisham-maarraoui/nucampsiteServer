17e1c7b4582985074a6871d8a926d6b6cd60c3db
Hisham <62486806+hisham-maarraoui@users.noreply.github.com>
2024-06-03 15:02:33 -0700

    added comments

--- authenticate.js
+++ authenticate.js
@@ -1,3 +1,10 @@
+
+// I worked on this code as part of a coding bootcamp curriculum. I followed along with the instructions 
+// (i.e. followed/copied instructions from the course/instructors and didn't design everything from scratch myself) while
+//  writing code in this project/file. Moreover, I  acknowledge receiving support from and/or working/collaborating
+//   with instructors/classmates, generally as is expected from being a participant in the coding bootcamp.
+
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,10 @@
+
+// I worked on this code as part of a coding bootcamp curriculum. I followed along with the instructions 
+// (i.e. followed/copied instructions from the course/instructors and didn't design everything from scratch myself) while
+//  writing code in this project/file. Moreover, I  acknowledge receiving support from and/or working/collaborating
+//   with instructors/classmates, generally as is expected from being a participant in the coding bootcamp.
+
+
 const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
@@ -20,7 +27,7 @@ router
       .catch((err) => next(err));
   });
 
-  
+
 // router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
 //   User.find({})
 //   .then( (users) => {
